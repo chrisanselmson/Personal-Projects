7739406966dfe674e197d47a745bec70eb385e9c
chrisanselmson <cralonsh8@gmail.com>
2022-10-06 12:58:41 +0200

    Add account login section

--- Store/JavaScript/app.js
+++ Store/JavaScript/app.js
@@ -2,7 +2,11 @@ const shoppingCartDiv = document.querySelector("#cart-div");
 const shoppingCart = document.querySelector("#shopping-cart");
 
 shoppingCart.addEventListener("click", ()=>{
-    if (shoppingCartDiv.style.display=="none") shoppingCartDiv.style.display="";
-    else shoppingCartDiv.style.display="none";
+    if (shoppingCartDiv.style.display=="none"){
+        shoppingCartDiv.style.display="block";
+    }
+    else{
+        shoppingCartDiv.style.display="none";
+    }
 });
 
